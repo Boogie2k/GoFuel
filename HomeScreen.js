16172c464b5073455c28aa62084ffa5a579fb699
Boogie2k <oghosabenjamin@gmail.com>
2023-06-23 10:14:11 +0100

    added the map

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,12 +1,53 @@
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Platform } from "react-native";
 import { StatusBar } from "react-native";
+import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 const HomeScreen = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  let [text, setText] = useState("witing");
+
+  const [region, setRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      if (errorMsg) {
+        setText(errorMsg);
+      } else if (location) {
+        setText(JSON.stringify(location));
+      }
+      setRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    })();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>HomeScreen</Text>
+      <MapView initialRegion={region} style={styles.map}>
+        <Marker coordinate={region} title="marker" />
+      </MapView>
+      <Text style={styles.paragraph}>{text}</Text>
     </SafeAreaView>
   );
 };
@@ -16,5 +57,10 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "80%",
   },
 });
